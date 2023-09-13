47ac6f679c3709b94e68b10673bd153b613ed009
Ford Pickert <fordpickert@gmail.com>
2023-09-13 17:30:39 -0500

    messing with looped query for pids

--- src/utils/pid_fetcher.py
+++ src/utils/pid_fetcher.py
@@ -6,11 +6,27 @@ SUBGRAPH_ENDPOINTS = {
     "arbitrum": "https://api.thegraph.com/subgraphs/name/sushiswap/arbitrum-minichef",
 }
 
+fields = [""]
+
+
 def fetch_pids():
     pid_query = """
     query pidQuery {
-        pools(first: 100) {
+        pools {
             id
+            pair
         }
     }
 """
+    pids = {}
+
+    for pid in pids:
+        result = requests.post(
+            SUBGRAPH_ENDPOINTS["arbitrum"],
+            json={"query": pid_query},
+        )
+
+        data = json.loads(result.text)
+        print(data)
+
+        return pids
